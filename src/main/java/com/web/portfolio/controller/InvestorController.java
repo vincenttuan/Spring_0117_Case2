5e291b2210db58e0fabf68d67fc05078551669d4
teacher <teacher@192.168.1.180>
2020-03-30 20:53:34 +0800

    Spring JPA

--- src/main/java/com/web/portfolio/controller/InvestorController.java
+++ src/main/java/com/web/portfolio/controller/InvestorController.java
@@ -5,7 +5,9 @@ import com.web.portfolio.service.PortfolioService;
 import java.util.Map;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +37,13 @@ public class InvestorController {
         investor.setPass(Boolean.TRUE);
         service.getInvestorRepository().save(investor);
         return investor;
+    }
+    
+    @DeleteMapping(value = {"/{id}", "/delete/{id}"})
+    @Transactional
+    public Boolean delete(@PathVariable("id") Long id) {
+        service.getInvestorRepository().delete(id);
+        return true;
     }
     
 }
