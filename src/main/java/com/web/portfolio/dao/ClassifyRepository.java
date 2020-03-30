0c920cc4ed669a9ae0e37f27d8ce621cec331917
teacher <teacher@192.168.1.180>
2020-03-30 21:21:16 +0800

    Spring JPA

--- src/main/java/com/web/portfolio/dao/ClassifyRepository.java
+++ src/main/java/com/web/portfolio/dao/ClassifyRepository.java
@@ -1,8 +1,6 @@
 package com.web.portfolio.dao;
 
 import com.web.portfolio.entity.Classify;
-import java.io.Serializable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -11,5 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Repository(value = "classifyRepository")
 public interface ClassifyRepository extends CrudRepository<Classify, Long>{
-    
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE Classify SET name=?2, transaction=?3 WHERE id=?1")
+    public void update(Long id, String name, Boolean transaction);
 }
