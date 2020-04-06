8a758d6a8cfe7d860704a53935b54e2ab7077ff7
teacher <teacher@192.168.1.180>
2020-04-06 20:00:17 +0800

    Spring JPA

--- src/main/java/com/web/portfolio/controller/PriceController.java
+++ src/main/java/com/web/portfolio/controller/PriceController.java
@@ -22,13 +22,32 @@ public class PriceController {
     @Autowired
     private PortfolioService service;
     
+    // 股價行情資料更新
     @GetMapping(value = {"/refresh"})
     @Transactional
     public Iterable<TStock> refresh() {
-        // Block of code
-        return null;
+        // 取得資料庫裡所有股票
+        Iterable<TStock> list = service.gettStockRepository().findAll();
+        for (TStock tStock : list) {
+            // 取得報價資訊
+            try {
+                Stock stock = YahooFinance.get(tStock.getSymbol());
+                tStock.setChangePrice(stock.getQuote().getChange()); // 漲跌
+                tStock.setChangeInPercent(stock.getQuote().getChangeInPercent()); // 漲跌幅%
+                tStock.setPreClosed(stock.getQuote().getPreviousClose()); // 昨收
+                tStock.setPrice(stock.getQuote().getPrice()); // 報價
+                tStock.setTransactionDate(stock.getQuote().getLastTradeTime().getTime()); // 交易時間
+                tStock.setVolumn(stock.getQuote().getVolume()); // 交易量
+                // 更新報價
+                service.gettStockRepository().save(tStock);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return list;
     }
     
+    // 歷史行情資料走勢
     @GetMapping(value = {"/histquotes/{symbol:.+}"}) // 歷史股價
     public List<HistoricalQuote> queryHistQuotes(@PathVariable("symbol") String symbol) {
         // Block of code
